263a752cd7718670dcd42d977fd84f0776a18ee2
Raddames <raddamestonui48@gmail.com>
2024-02-27 00:29:30 +0300

    First commit

---
+++ main.dart
@@ -0,0 +1,7 @@
+void main(){ //this is the syntsx of dart
+
+    print("Hello World! This is my first Dart programme");  
+    /* this is used as a 
+    multiline comment*/
+
+}
---
+++ variables.dart
@@ -0,0 +1,8 @@
+
+void main(){
+  var name="Raddames Tonui";
+  String sname= "Kiplangat";
+  int age = 24;
+  print(name + " "+ sname);
+
+}
